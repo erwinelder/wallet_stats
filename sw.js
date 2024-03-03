7b7200b132863947856fcb904951f6bd4e0f57be
Erwin Elder <erwineldermail@gmail.com>
2024-03-03 15:39:42 +0100

    Bug fixing.

--- sw.js
+++ sw.js
@@ -1,3 +1,4 @@
+const cache_name_prefix = 'app-cache-';
 const cache_version = 'v3.2.2';
 const asset_files = [
     'index.html',
@@ -21,7 +22,16 @@ self.addEventListener('install', async () => {
 
 self.addEventListener('activate', async () => {
     const cache_names = await caches.keys();
+    const current_cache = cache_names.find(name => name.startsWith(cache_name_prefix));
+
     console.log(cache_names);
+
+    if (!current_cache) {
+        // Create a new cache with the correct name and add files
+        const cache = await caches.open(`${cache_name_prefix}${cache_version}`);
+        await cache.addAll(asset_files);
+    }
+
     await Promise.all(
         cache_names
             .filter(name => name !== cache_version)
